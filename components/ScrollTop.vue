7b5160eb5a31348dc6c697f47ff6b5e30782bc1c
douglascustodiodesiner <94208514+douglascustodiodesigner@users.noreply.github.com>
2021-12-18 19:32:53 -0400

    Recuo

--- components/ScrollTop.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <client-only>
-        <back-to-top class="scroll-top" bottom="30px">
-            <i class="ion-arrow-up-c"></i>
-        </back-to-top>
-    </client-only>
-</template>
